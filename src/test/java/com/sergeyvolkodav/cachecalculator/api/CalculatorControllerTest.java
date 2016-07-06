74aa47a820d6abe58a14fc761454f436c061a58f
Sergey Volkodav <volkodav_s@yahoo.com>
2016-07-06 01:05:04 +0100

    Add api test

--- src/test/java/com/sergeyvolkodav/cachecalculator/api/CalculatorControllerTest.java
+++ src/test/java/com/sergeyvolkodav/cachecalculator/api/CalculatorControllerTest.java
@@ -1,65 +1,133 @@
 package com.sergeyvolkodav.cachecalculator.api;
 
-import com.jayway.restassured.module.mockmvc.RestAssuredMockMvc;
+import com.jayway.restassured.RestAssured;
 import com.sergeyvolkodav.cachecalculator.Application;
-import org.junit.BeforeClass;
-import org.junit.Ignore;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
-import javax.servlet.http.HttpServletResponse;
-
-import static com.jayway.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static com.jayway.restassured.RestAssured.when;
 import static org.hamcrest.Matchers.equalTo;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(Application.class)
-@IntegrationTest
 @WebAppConfiguration
+@IntegrationTest("server.port:0")
 public class CalculatorControllerTest {
 
+    @Value("${local.server.port}")
+    int port;
 
-    @BeforeClass
-    public static void setupURL() {
-        RestAssuredMockMvc.basePath = "/rest/v1/calculator";
-
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+        RestAssured.basePath = "/rest/v1/calculator";
     }
 
     @Test
-    @Ignore
     public void add() throws Exception {
-        given().
-                contentType("application/json").
-                when().
+        when().
                 get("/add/1/2/97").
                 then().
-                statusCode(HttpServletResponse.SC_OK).
+                statusCode(HttpStatus.SC_OK).
                 contentType("application/json").
-                body("result", equalTo("100"));
+                body("result", equalTo("100.00"));
+    }
+
+    @Test
+    public void addOneValue() throws Exception {
+        when().
+                get("/add/1").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("1.00"));
     }
 
     @Test
     public void subtract() throws Exception {
+        when().
+                get("/subtract/200/150.20").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("49.80"));
+    }
 
+    @Test
+    public void subtractOneValue() throws Exception {
+        when().
+                get("/subtract/1").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("1.00"));
     }
 
     @Test
     public void multiply() throws Exception {
+        when().
+                get("/multiply/2.02/-0,1/100").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("-20.200000"));
+    }
 
+    @Test
+    public void multiplyOneValue() throws Exception {
+        when().
+                get("/multiply/2.1").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("2.10"));
     }
 
     @Test
     public void divide() throws Exception {
+        when().
+                get("/divide/8/4/2").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("1.00"));
+    }
 
+    @Test
+    public void divideOneValue() throws Exception {
+        when().
+                get("/divide/8").
+                then().
+                statusCode(HttpStatus.SC_OK).
+                contentType("application/json").
+                body("result", equalTo("8.00"));
     }
 
     @Test
     public void handleUncheckedExceptions() throws Exception {
+        when().
+                get("/divide/8/0").
+                then().
+                statusCode(HttpStatus.SC_BAD_REQUEST).
+                contentType("application/json").
+                body("status", equalTo("error"));
+    }
 
+    @Test
+    public void handleUncheckedExceptionsToManyValues() throws Exception {
+        when().
+                get("/divide/8/2/2/2").
+                then().
+                statusCode(HttpStatus.SC_BAD_REQUEST).
+                contentType("application/json").
+                body("status", equalTo("error"));
     }
 }
